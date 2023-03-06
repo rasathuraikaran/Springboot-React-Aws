dcf5669968f3e7da544e55d0c0c57005f349e3dc
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2023-03-06 20:03:33 +0530

    Update AmazonConfig.java

--- src/main/java/com/karan/awsimageuploader/config/AmazonConfig.java
+++ src/main/java/com/karan/awsimageuploader/config/AmazonConfig.java
@@ -14,7 +14,7 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3(){
       AWSCredentials awsCredentials=null;
-        awsCredentials=new   BasicAWSCredentials ("++++++++++","++++++++++++");
+        awsCredentials=new   BasicAWSCredentials ("++++++++++++++","++++++++++++");
    return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
     }
 }
